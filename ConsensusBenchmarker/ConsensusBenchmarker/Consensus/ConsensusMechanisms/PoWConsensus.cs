3637dbea663667ba869d08dbc68190e085f5801c
JeppeKLau <44167691+JeppeKLau@users.noreply.github.com>
2022-12-21 14:12:11 +0100

    Update PoWConsensus.cs

--- ConsensusBenchmarker/ConsensusBenchmarker/Consensus/ConsensusMechanisms/PoWConsensus.cs
+++ ConsensusBenchmarker/ConsensusBenchmarker/Consensus/ConsensusMechanisms/PoWConsensus.cs
@@ -9,7 +9,7 @@ namespace ConsensusBenchmarker.Consensus.PoW
 {
     public class PoWConsensus : ConsensusDriver
     {
-        private readonly uint DifficultyLeadingZeroes = 6;
+        private readonly uint DifficultyLeadingZeroes = 7;
         private bool allowMining;
         private bool restartMining;
         private readonly Random random;
